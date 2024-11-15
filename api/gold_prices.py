9f84b319de99b6fc54899bfb7273462562a28412
giraldi07 <giraldipramay@gmail.com>
2024-11-15 08:29:41 +0700

    resolve problem CORS

--- api/gold_prices.py
+++ api/gold_prices.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)  # Menambahkan CORS di seluruh aplikasi
 
 # Fungsi untuk scraping data harga emas
 def get_gold_prices():
@@ -10,7 +12,6 @@ def get_gold_prices():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Mengambil data berdasarkan selector yang sudah dijelaskan
     tanggal = soup.select('tr.title_table:nth-child(1) td')[0].text.strip()
     judul_kolom = [td.text.strip() for td in soup.select('tr.title_table:nth-child(2) td')]
     harga = [td.text.strip() for td in soup.select('tr:nth-child(4) td')]
@@ -23,16 +24,10 @@ def get_gold_prices():
         'waktu': waktu
     }
 
-@app.route('/')
-def home():
-    return "Welcome to Scrap Gold API!"
-
-
-# Endpoint API untuk mendapatkan data harga emas
 @app.route('/api/gold_prices', methods=['GET'])
 def api_gold_prices():
     gold_data = get_gold_prices()
-    return jsonify(gold_data)  # Mengembalikan data dalam format JSON
+    return jsonify(gold_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
